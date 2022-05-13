55a7e2c7f36669ceb654f80afa0330a46fde74b3
yalincode <yukselyal@gmail.com>
2022-05-13 15:17:54 +0300

    EfCore entegration and connect with database Changed all methods for database entegration

--- SuperHeroAPI/Controllers/SuperHeroController.cs
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -15,7 +15,11 @@ namespace SuperHeroAPI.Controllers
                 new SuperHero{Id=3,Name="Dr Strange",FirstName="Stephan",LastName="Strange",Place="Sanctum Sanctorum"}
             };
 
-
+        private readonly DataContext _dataContext;
+        public SuperHeroController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
         //[HttpGet]
         //public async Task<IActionResult> Get() //We cannot show the schemas in swegger because it is IActionresult interface.
         //{
@@ -30,20 +34,21 @@ namespace SuperHeroAPI.Controllers
         [HttpGet]
         public async Task<ActionResult<List<SuperHero>>> Get() //we can show the schemas
         {
-            return Ok(heroes);
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
         }
 
         [HttpPost]
         public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
         {
-            heroes.Add(hero);
-            return Ok(heroes);
+            _dataContext.SuperHeroes.Add(hero);
+            await _dataContext.SaveChangesAsync();  
+            return Ok(await _dataContext.SuperHeroes.ToListAsync());
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<SuperHero>> Get(int id) //we can show the schemas
         {
-            var hero = heroes.Find(x=>x.Id==id);
+            var hero =await _dataContext.SuperHeroes.FindAsync(id);
             if (hero==null)
             {
                 return BadRequest("Hero not found.");
@@ -53,23 +58,25 @@ namespace SuperHeroAPI.Controllers
                 return Ok(hero);
             }
             
-        }
+        }          
 
         [HttpPut]
         public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
         {
-            var hero = heroes.Find(x => x.Id == request.Id);
-            if (hero == null)
+            var dbHero = await _dataContext.SuperHeroes.FindAsync(request.Id);
+            if (dbHero == null)
             {
                 return BadRequest("Hero not found.");
             }
             else
             {
-                hero.Name = request.Name;
-                hero.FirstName = request.FirstName;
-                hero.LastName = request.LastName;
-                hero.Place = request.Place;
-                return Ok(heroes);
+                dbHero.Name = request.Name;
+                dbHero.FirstName = request.FirstName;
+                dbHero.LastName = request.LastName;
+                dbHero.Place = request.Place;
+
+                await _dataContext.SaveChangesAsync();
+                return Ok(await _dataContext.SuperHeroes.ToListAsync());
             }
             
         }
@@ -77,15 +84,16 @@ namespace SuperHeroAPI.Controllers
         [HttpDelete("{id}")]
         public async Task<ActionResult<List<SuperHero>>> DeleteHero(int id)
         {
-            var hero = heroes.Find(x => x.Id == id);
+            var hero = await _dataContext.SuperHeroes.FindAsync(id);
             if (hero == null)
             {
                 return BadRequest("Hero not found.");
             }
             else
             {
-                heroes.Remove(hero);
-                return Ok(heroes);
+                _dataContext.SuperHeroes.Remove(hero);
+                await _dataContext.SaveChangesAsync();
+                return Ok(await _dataContext.SuperHeroes.ToListAsync());
             }
         }
     }
---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI;
+
+namespace Data.SuperHeroAPI
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
